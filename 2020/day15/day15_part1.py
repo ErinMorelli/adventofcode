7d637adb7e6ed278c50aed4c9d91f0ea451ea3fb
Erin Morelli <ermorell@amazon.com>
2020-12-15 17:20:11 -0500

    wip

--- 2020/day15/day15_part1.py
+++ 2020/day15/day15_part1.py
@@ -5,5 +5,34 @@
 input_file = 'input.txt'
 
 with open(input_file, 'r') as fh:
-    raw_data = fh.read().splitlines()
+    raw_data = fh.read().strip().split(',')
 
+data = [int(n, 10) for n in raw_data]
+
+turns = data.copy()
+spoken = set(turns[:-1])
+turn = len(data)
+current = data[-1]
+
+
+def find_index(num, history):
+    for i in range(len(history)-2, -1, -1):
+        if history[i] == num:
+            return i + 1
+    return 0
+
+
+while turn <= 2020:
+    if turn == 2020:
+        print(f'[{turn}] {current}')
+
+    if current in spoken:
+        spoken.add(current)
+        prev_idx = find_index(current, turns)
+        current = turn - prev_idx
+    else:
+        spoken.add(current)
+        current = 0
+
+    turns.append(current)
+    turn += 1
--- 2020/day15/day15_part2.py
+++ 2020/day15/day15_part2.py
@@ -5,5 +5,33 @@
 input_file = 'input.txt'
 
 with open(input_file, 'r') as fh:
-    raw_data = fh.read().splitlines()
+    raw_data = fh.read().strip().split(',')
 
+data = [int(n, 10) for n in raw_data]
+
+spoken = set(data[:-1])
+turn = len(data)
+current = data[-1]
+turn_map = dict()
+
+
+for idx, start_num in enumerate(data):
+    turn_map[start_num] = [idx+1]
+
+
+while turn <= 30000000:
+    if turn == 30000000:
+        print(f'[{turn}] {current}')
+        break
+
+    if current in spoken:
+        spoken.add(current)
+        prev_idx = turn_map[current][-1]
+        turn_map[current].append(turn)
+        current = turn - prev_idx
+    else:
+        spoken.add(current)
+        turn_map[current] = [turn]
+        current = 0
+
+    turn += 1

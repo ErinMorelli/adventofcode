c6b0aca8a25ed48f284c501a1ecae3807b0d10c1
Erin Morelli <me@erin.dev>
2020-12-16 21:09:03 -0500

    wip

--- 2020/day16/day16_part1.py
+++ 2020/day16/day16_part1.py
@@ -1,9 +1,116 @@
 #!/usr/bin/env python
 """
-???
+As you're walking to yet another connecting flight, you realize that one of
+the legs of your re-routed trip coming up is on a high-speed train. However,
+the train ticket you were given is in a language you don't understand. You
+should probably figure out what it says before you get to the train station
+after the next flight.
+
+Unfortunately, you can't actually read the words on the ticket. You can,
+however, read the numbers, and so you figure out the fields these tickets must
+have and the valid ranges for values in those fields.
+
+You collect the rules for ticket fields, the numbers on your ticket, and the
+numbers on other nearby tickets for the same train service (via the airport
+security cameras) together into a single document you can reference (your
+puzzle input).
+
+The rules for ticket fields specify a list of fields that exist somewhere on
+the ticket and the valid ranges of values for each field. For example, a rule
+like `class: 1-3 or 5-7` means that one of the fields in every ticket is named
+`class` and can be any value in the ranges `1-3` or `5-7` (inclusive, such
+that `3` and `5` are both valid in this field, but `4` is not).
+
+Each ticket is represented by a single line of comma-separated values. The
+values are the numbers on the ticket in the order they appear; every ticket
+has the same format. For example, consider this ticket:
+
+    .--------------------------------------------------------.
+    | ????: 101    ?????: 102   ??????????: 103     ???: 104 |
+    |                                                        |
+    | ??: 301  ??: 302             ???????: 303      ??????? |
+    | ??: 401  ??: 402           ???? ????: 403    ????????? |
+    '--------------------------------------------------------'
+
+Here, `?` represents text in a language you don't understand. This ticket
+might be represented as `101,102,103,104,301,302,303,401,402,403`; of course,
+the actual train tickets you're looking at are much more complicated. In any
+case, you've extracted just the numbers in such a way that the first number is
+always the same specific field, the second number is always a different
+specific field, and so on - you just don't know what each position actually
+means!
+
+Start by determining which tickets are completely invalid; these are tickets
+that contain values which aren't valid for any field. Ignore your ticket for
+now.
+
+For example, suppose you have the following notes:
+
+    class: 1-3 or 5-7
+    row: 6-11 or 33-44
+    seat: 13-40 or 45-50
+
+    your ticket:
+    7,1,14
+
+    nearby tickets:
+    7,3,47
+    40,4,50
+    55,2,20
+    38,6,12
+
+It doesn't matter which position corresponds to which field; you can identify
+invalid nearby tickets by considering only whether tickets contain values that
+are not valid for any field. In this example, the values on the first nearby
+ticket are all valid for at least one field. This is not true of the other
+three nearby tickets: the values `4`, `55`, and `12` are are not valid for any
+field. Adding together all of the invalid values produces your ticket scanning
+error rate: `4 + 55 + 12` = `71`.
+
+Consider the validity of the nearby tickets you scanned. What is your ticket
+scanning error rate?
 """
+import re
+
 input_file = 'input.txt'
 
 with open(input_file, 'r') as fh:
-    raw_data = fh.read().splitlines()
+    raw_data = fh.read().strip()
 
+data = raw_data.split('your ticket:\n')
+data2 = data[1].split('nearby tickets:\n')
+
+fields = {}
+raw_fields = data[0].strip().split('\n')
+field_regex = r'(?P<fname>.+): (?P<rangeA1>\d+)-(?P<rangeA2>\d+) or ' \
+              r'(?P<rangeB1>\d+)-(?P<rangeB2>\d+)'
+
+for f in raw_fields:
+    match = re.match(field_regex, f)
+    fname = match.group('fname')
+    fields[fname] = [
+        [int(match.group('rangeA1')), int(match.group('rangeA2'))],
+        [int(match.group('rangeB1')), int(match.group('rangeB2'))]
+    ]
+
+raw_nearby_tickets = data2[1].strip().split('\n')
+nearby_tickets = [[int(i) for i in t.split(',')] for t in raw_nearby_tickets]
+
+invalid_values = []
+
+for ticket in nearby_tickets:
+    for value in ticket:
+        match = False
+
+        for field in fields.values():
+            range1 = range(field[0][0], field[0][1]+1)
+            range2 = range(field[1][0], field[1][1]+1)
+
+            if value in range1 or value in range2:
+                match = True
+                break
+
+        if not match:
+            invalid_values.append(value)
+
+print(f'error rate: {sum(invalid_values)}')
--- 2020/day16/day16_part2.py
+++ 2020/day16/day16_part2.py
@@ -1,9 +1,144 @@
 #!/usr/bin/env python
 """
-???
+Now that you've identified which tickets contain invalid values, discard those
+tickets entirely. Use the remaining valid tickets to determine which field is
+which.
+
+Using the valid ranges for each field, determine what order the fields appear
+on the tickets. The order is consistent between all tickets: if `seat` is the
+third field, it is the third field on every ticket, including your ticket.
+
+For example, suppose you have the following notes:
+
+    class: 0-1 or 4-19
+    row: 0-5 or 8-19
+    seat: 0-13 or 16-19
+
+    your ticket:
+    11,12,13
+
+    nearby tickets:
+    3,9,18
+    15,1,5
+    5,14,9
+
+Based on the nearby tickets in the above example, the first position must be
+`row`, the second position must be `class`, and the third position must be
+`seat`; you can conclude that in your ticket, `class` is `12`, `row` is `11`,
+and `seat` is `13`.
+
+Once you work out which field is which, look for the six fields on your ticket
+that start with the word `departure`. What do you get if you multiply those
+six values together?
 """
+import re
+from math import prod
+
 input_file = 'input.txt'
 
 with open(input_file, 'r') as fh:
-    raw_data = fh.read().splitlines()
+    raw_data = fh.read().strip()
 
+data = raw_data.split('your ticket:\n')
+data2 = data[1].split('nearby tickets:\n')
+
+fields = {}
+raw_fields = data[0].strip().split('\n')
+field_regex = r'(?P<fname>.+): (?P<rangeA1>\d+)-(?P<rangeA2>\d+) or ' \
+              r'(?P<rangeB1>\d+)-(?P<rangeB2>\d+)'
+
+for f in raw_fields:
+    match = re.match(field_regex, f)
+    fname = match.group('fname')
+    fields[fname] = [
+        [int(match.group('rangeA1')), int(match.group('rangeA2'))],
+        [int(match.group('rangeB1')), int(match.group('rangeB2'))]
+    ]
+
+raw_ticket = data2[0].strip().split(',')
+your_ticket = [int(i) for i in raw_ticket]
+
+raw_nearby_tickets = data2[1].strip().split('\n')
+nearby_tickets = [[int(i) for i in t.split(',')] for t in raw_nearby_tickets]
+
+
+def validate_field(val, ranges):
+    range1 = range(ranges[0][0], ranges[0][1] + 1)
+    range2 = range(ranges[1][0], ranges[1][1] + 1)
+
+    return val in range1 or value in range2
+
+
+valid_tickets = [your_ticket]
+
+for ticket in nearby_tickets:
+    valid = True
+    for value in ticket:
+        match = False
+        for field in fields.values():
+            if validate_field(value, field):
+                match = True
+                break
+        if not match:
+            valid = False
+            break
+    if valid:
+        valid_tickets.append(ticket)
+
+
+possible_fields = [{f: True for f in fields.keys()}
+                   for _ in range(len(your_ticket))]
+
+for ticket in valid_tickets:
+    for idx, value in enumerate(ticket):
+        for fname, fvalues in fields.items():
+            if not possible_fields[idx][fname]:
+                continue
+            if not validate_field(value, fvalues):
+                possible_fields[idx][fname] = False
+
+
+possible_fields_by_index = [set() for _ in range(len(your_ticket))]
+
+for idx, pf in enumerate(possible_fields):
+    for fname, valid in pf.items():
+        if valid:
+            possible_fields_by_index[idx].add(fname)
+
+matched_fields_by_index = {f: None for f in fields.keys()}
+
+
+def match_fields(to_match, matched):
+    for i, pf in enumerate(to_match):
+        if not len(pf):
+            continue
+
+        field_name = list(pf)[0]
+
+        if len(pf) == 1 and matched[field_name] is None:
+            matched[field_name] = i
+
+            for j, pf2 in enumerate(to_match):
+                if i == j:
+                    to_match[j] = []
+                    
+                if field_name in pf2:
+                    to_match[j] = [p for p in pf2 if p != field_name]
+            break
+
+    if None in matched.values():
+        return match_fields(to_match, matched)
+
+    return matched
+
+
+fields_by_index = match_fields(possible_fields_by_index,
+                               matched_fields_by_index)
+
+ticket_values = {}
+
+for field, idx in fields_by_index.items():
+    if field.startswith('departure'):
+        ticket_values[field] = your_ticket[idx]
+
+print(f'product: {prod(ticket_values.values())}')

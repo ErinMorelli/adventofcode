1e1173dba9b97fbfbfdb1d2054fe267cf19fede4
Erin Morelli <me@erin.dev>
2022-12-07 23:33:40 -0500

    2021 Day 11

---
+++ 2021/day11/day11_part2.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+"""
+???
+"""
+input_file = 'input.txt'
+
+with open(input_file, 'r') as fh:
+    raw_data = fh.read().splitlines()
+
+data = [[int(y, 10) for y in x] for x in raw_data]
+
+flashed = set()
+
+
+def flash(x, y):
+    if (x, y) in flashed:
+        return
+
+    flashed.add((x, y))
+
+    # north
+    if y-1 >= 0 and (x, y-1) not in flashed:
+        data[y-1][x] += 1
+        if data[y-1][x] > 9:
+            flash(x, y-1)
+    # northeast
+    if y-1 >= 0 and x+1 < len(data[y]) and (x+1, y-1) not in flashed:
+        data[y-1][x+1] += 1
+        if data[y-1][x+1] > 9:
+            flash(x+1, y-1)
+    # east
+    if x+1 < len(data[y]) and (x+1, y) not in flashed:
+        data[y][x+1] += 1
+        if data[y][x+1] > 9:
+            flash(x+1, y)
+    # southeast
+    if y+1 < len(data) and x+1 < len(data[y]) and (x+1, y+1) not in flashed:
+        data[y+1][x+1] += 1
+        if data[y+1][x+1] > 9:
+            flash(x+1, y+1)
+    # south
+    if y+1 < len(data) and (x, y+1) not in flashed:
+        data[y+1][x] += 1
+        if data[y+1][x] > 9:
+            flash(x, y+1)
+    # southwest
+    if y+1 < len(data) and x-1 >= 0 and (x-1, y+1) not in flashed:
+        data[y+1][x-1] += 1
+        if data[y+1][x-1] > 9:
+            flash(x-1, y+1)
+    # west
+    if x-1 >= 0 and (x-1, y) not in flashed:
+        data[y][x-1] += 1
+        if data[y][x-1] > 9:
+            flash(x-1, y)
+    # northwest
+    if y-1 >= 0 and x-1 >= 0 and (x-1, y-1) not in flashed:
+        data[y-1][x-1] += 1
+        if data[y-1][x-1] > 9:
+            flash(x-1, y-1)
+
+    data[y][x] = 0
+
+
+def step():
+    to_flash = []
+    for y, row in enumerate(data):
+        for x, _ in enumerate(row):
+            data[y][x] += 1
+            if data[y][x] > 9:
+                to_flash.append((x, y))
+
+    for x, y in to_flash:
+        flash(x, y)
+
+
+i = 1
+
+while True:
+    step()
+    if len(flashed) == 100:
+        break
+    flashed.clear()
+    i += 1
+
+print(i)

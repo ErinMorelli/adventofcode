cd14d82f1958bca15e3455f6e6a7cec3437ea453
Erin Morelli <me@erin.dev>
2023-12-11 21:06:25 -0500

    2023 Day 10, partial

--- 2023/day8/day8_part1.py
+++ 2023/day8/day8_part1.py
@@ -17,9 +17,9 @@ It seems like you're meant to use the left/right instructions to navigate the
 network. Perhaps if you have the camel follow the same instructions, you can
 escape the haunted wasteland!
 
-After examining the maps for a bit, two nodes stick out: AAA and ZZZ. You feel
-like AAA is where you are now, and you have to follow the left/right
-instructions until you reach ZZZ.
+After examining the maps for a bit, two nodes stick out: `AAA` and `ZZZ`. You
+feel like `AAA` is where you are now, and you have to follow the left/right
+instructions until you reach `ZZZ`.
 
 This format defines each node of the network individually. For example:
 
@@ -33,16 +33,16 @@ This format defines each node of the network individually. For example:
     GGG = (GGG, GGG)
     ZZZ = (ZZZ, ZZZ)
 
-Starting with AAA, you need to look up the next element based on the next
-left/right instruction in your input. In this example, start with AAA and go
-right (R) by choosing the right element of AAA, CCC. Then, L means to choose
-the left element of CCC, ZZZ. By following the left/right instructions, you
-reach ZZZ in 2 steps.
+Starting with `AAA`, you need to look up the next element based on the next
+left/right instruction in your input. In this example, start with `AAA` and go
+right (`R`) by choosing the right element of `AAA`, `CCC`. Then, `L` means to
+choose the left element of `CCC`, `ZZZ`. By following the left/right
+instructions, you reach `ZZZ` in `2` steps.
 
-Of course, you might not find ZZZ right away. If you run out of left/right
-instructions, repeat the whole sequence of instructions as necessary: RL
-really means RLRLRLRLRLRLRLRL... and so on. For example, here is a situation
-that takes 6 steps to reach ZZZ:
+Of course, you might not find `ZZZ` right away. If you run out of left/right
+instructions, repeat the whole sequence of instructions as necessary: `RL`
+really means `RLRLRLRLRLRLRLRL...` and so on. For example, here is a situation
+that takes `6` steps to reach `ZZZ`:
 
     LLR
 
@@ -50,9 +50,8 @@ that takes 6 steps to reach ZZZ:
     BBB = (AAA, ZZZ)
     ZZZ = (ZZZ, ZZZ)
 
-Starting at AAA, follow the left/right instructions. How many steps are
-required to reach ZZZ?
-
+Starting at `AAA`, follow the left/right instructions. How many steps are
+required to reach `ZZZ`?
 """
 from re import sub
 
